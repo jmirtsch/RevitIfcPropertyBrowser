29cef23562e0dd5990afeed1642e44f88e815186
jmirtsch <jonm@geometrygym.com>
2017-12-08 20:39:56 +1100

    First Commit

---
+++ RevitIfcPropertyBrowser/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.InteropServices;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("IFC Property Browser for Revit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Autodesk, Inc.")]
+[assembly: AssemblyProduct("IFC Property Browser for Revit")]
+[assembly: AssemblyCopyright("© 2012-2017 Autodesk, Inc. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8CA52777-B76B-46A7-A2E0-3637BB6C1F32")]
---
+++ RevitIfcPropertyBrowser/browser.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+
+using Autodesk.Revit.UI;
+
+namespace RevitIfcPropertyBrowser
+{
+	public partial class Browser : Page, IDockablePaneProvider
+	{
+		internal PropertyGrid mPropertyGrid = null;
+		public Browser()
+		{
+			InitializeComponent();
+			mPropertyGrid = new PropertyGrid();
+			System.Windows.Forms.TextBox tb = new System.Windows.Forms.TextBox() { Text = "jon" };
+			WindowsFormsHost host = new WindowsFormsHost() { Child = mPropertyGrid };
+			grid.Children.Add(host);
+		}
+
+		public void SetupDockablePane(DockablePaneProviderData data)
+		{
+			data.FrameworkElement = this as FrameworkElement;
+			data.InitialState = new DockablePaneState();
+			data.InitialState.DockPosition = DockPosition.Tabbed;
+			//DockablePaneId targetPane;
+			//if (m_targetGuid == Guid.Empty)
+			//    targetPane = null;
+			//else targetPane = new DockablePaneId(m_targetGuid);
+			//if (m_position == DockPosition.Tabbed)
+
+			data.InitialState.TabBehind = Autodesk.Revit.UI.DockablePanes.BuiltInDockablePanes.ProjectBrowser;
+			//if (m_position == DockPosition.Floating)
+			//{
+			//data.InitialState.SetFloatingRectangle(new Autodesk.Revit.UI.Rectangle(10, 710, 10, 710));
+			//data.InitialState.DockPosition = DockPosition.Tabbed;
+			//}
+			//Log.Message("***Intial docking parameters***");
+			//Log.Message(APIUtility.GetDockStateSummary(data.InitialState));
+		}
+	}
+}
